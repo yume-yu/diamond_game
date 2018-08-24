db06216a8b5f248909dc33a8afac4a11d8d2501a
松井研究室 <matsuilab@matsuilab114.local>
2018-08-24 15:48:46 +0900

    Initial Commit

---
+++ diamond game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  diamond game
+//
+//  Created by Matsui Lab on 2018/08/24.
+//  Copyright © 2018年 kohei uno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
