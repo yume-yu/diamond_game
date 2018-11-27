d30051033cb8f0bbd6e835d27d39cb7b67f78a92
KoheUno <kohegmonhan@icloud.com>
2018-11-27 18:29:26 +0900

    EditWindowを作成

---
+++ diamond game/EditWindow.swift
@@ -0,0 +1,21 @@
+//
+//  EditWindow.swift
+//  diamond game
+//
+//  Created by Matsui Lab on 2018/08/24.
+//  Copyright © 2018年 kohei uno. All rights reserved.
+//
+
+import UIKit
+
+class EditWindow: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
