4c4c21cdf98f5a43f3027521b162569c36777539
KoheUno <kohegmonhan@icloud.com>
2018-12-01 11:49:39 +0900

    Team型を作った

---
+++ diamond game/Team.swift
@@ -0,0 +1,16 @@
+//
+//  Team.swift
+//  diamond game
+//
+//  Created by Matsui Lab on 2018/12/01.
+//  Copyright © 2018年 kohei uno. All rights reserved.
+//
+
+import Foundation
+
+enum Team {
+    case blue
+    case red
+    case yellow
+    case nai
+}
