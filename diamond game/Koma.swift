71e7d61e2c3c0886dfa259912a6f5c88bc746fbb
KoheUno <kohegmonhan@icloud.com>
2018-12-02 02:47:39 +0900

    Komaクラスに自分がタッチされたかを判定する関数を追加

--- diamond game/Koma.swift
+++ diamond game/Koma.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2018年 kohei uno. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 class Koma{
@@ -26,8 +25,17 @@ class Koma{
         self.y = y
         self.team = team
         self.mitame = Mitame.getMitame(x: x, y: y, team: team)
-        view.layer.addSublayer(mitame)
+        view.layer.insertSublayer(mitame, at: 0)
+        //view.layer.addSublayer(mitame)
         }
+    
+    init(){
+        x = 0;
+        y = 0;
+        team = Team.nai;
+        mitame = Mitame.getMitame(x: 0, y: 0, team: Team.nai)
+    }
+    
     /*コマの状態の更新
      引数:新しくなるチーム
     ex 無い状態 -> 青
@@ -43,6 +51,31 @@ class Koma{
         return (x:self.x,y:self.y)
     }
     
+    /**
+        自分がタッチされたかどうかを判断する関数
+        引数: タッチされた座標(CGPoint型)
+        戻り値: 自分がタッチされてたとき　-> 自分自身(Koma型)
+               自分がタッチされてなかった -> nul
+     **/
+    func hitTest(touchedPoint: CGPoint) -> Koma? {
+        //タッチされた点と自分の座標の距離をとる
+        let distance: CGFloat = pow(pow(CGFloat(self.x + Mitame.width/2) - touchedPoint.x, 2) + pow(CGFloat(self.y + Mitame.height/2) - touchedPoint.y, 2), 1/2)
+        //距離が20以下 / 自分の円の範囲内に収まっていたら自分を返す
+        if(distance <= 20){
+            return self;
+        }
+        //そうじゃなかったらnilを返す
+        return nil;
+    }
     
+    func switchSelected(){
+        self.mitame.strokeColor = UIColor.green.cgColor
+        self.mitame.lineWidth = 5;
+    }
+    
+    func switchUnSelected(){
+        self.mitame.strokeColor = UIColor.black.cgColor
+        self.mitame.lineWidth = 1;
+    }
 }
 
