b8c6a9dd501812d0bd9ab861bdad8ee0f367114a
KoheUno <kohegmonhan@icloud.com>
2018-12-01 11:18:02 +0900

    Komaクラスの作成

---
+++ diamond game/Koma.swift
@@ -0,0 +1,13 @@
+//
+//  Koma.swift
+//  diamond game
+//
+//  Created by Matsui Lab on 2018/12/01.
+//  Copyright © 2018年 kohei uno. All rights reserved.
+//
+
+import Foundation
+
+class Koma{
+   // <#code#>
+}
