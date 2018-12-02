1c2b423b16f44c3d907aafd14f09ce060fde07c8
KoheUno <kohegmonhan@icloud.com>
2018-12-02 09:03:17 +0900

    方向を示すenum型を作成した

---
+++ diamond game/Direction.swift
@@ -0,0 +1,16 @@
+//
+//  Direction.swift
+//  diamond game
+//
+//  Created by Matsui Lab on 2018/12/02.
+//  Copyright © 2018年 kohei uno. All rights reserved.
+//
+
+enum Direction{
+    case rightfront
+    case leftfront
+    case right
+    case left
+    case rightback
+    case leftback
+}
